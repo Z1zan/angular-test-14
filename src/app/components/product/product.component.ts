b19904170ad8122e3cb921f6c543bb8efac07a73
Nemirich Ivan <bebded@gmail.com>
2022-07-22 16:08:53 +0300

    add: prodicts model + product components with styles

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IProduct } from '../../models/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  @Input() product: IProduct;
+
+  details = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  showDetails() {
+    this.details = !this.details;
+  }
+}
